a53f8253863efe43eee6f55169633140e0f4d4d6
Calvin Oktavialdy Setia <Calvin2.Setia@live.uwe.ac.uk>
2023-02-23 03:09:02 +0000

    camera based movement

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,42 +1,100 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CameraScript : MonoBehaviour
 {
     // public
+    public LayerMask CollisionLayers;
     
     // private
     private GameObject _player;
-    private Vector3 _offset;
+    private GameObject _cameraPivot;
+    private GameObject _camera;
 
-    private Vector3 _desiredAngle;
-    private Vector3 _desiredPos;
-    private Vector3 _targetPos;
+    private Vector3 _followSpeed = Vector3.zero;
+    private float _followTime = 0.0f;
+    
+    private PlayerInputManager _playerInputManager;
 
-    void Start()
+    private float _cameraRotateSpeed = 0.8f;
+    
+    private float _rotateX;
+    private float _rotateY;
+
+    private float _minYAngle = -45;
+    private float _maxYAngle = 45;
+
+    private float _defaultZPosition;
+    private float _cameraCollisionRadius = 2;
+    private float _cameraCollisionOffset = 0.2f;
+    private float _minimumCollisionOffset = 0.2f;
+
+    private Vector3 _cameraVectorPosition;
+
+    private void Awake()
     {
         _player = GameObject.FindGameObjectWithTag("Player");
+        _cameraPivot = GameObject.FindGameObjectWithTag("Camera Pivot");
+        _camera = GameObject.FindGameObjectWithTag("MainCamera");
 
-        _offset = new Vector3(0.0f, 3.0f, -3.0f);
-
-        _desiredAngle = new Vector3();
+        _defaultZPosition = _camera.transform.localPosition.z;
+        
+        _playerInputManager = FindObjectOfType<PlayerInputManager>();
     }
-    
-    void FixedUpdate()
-    {
-        var look = _player.GetComponent<PlayerController>().look;
-        
-        _desiredAngle.x = _player.transform.eulerAngles.x + look.y * 30;
-        _desiredAngle.y = _player.transform.eulerAngles.y;
 
-        Quaternion rotation = Quaternion.Euler(_desiredAngle.x, _desiredAngle.y, 0);
-        _desiredPos = _player.transform.position + (rotation * _offset);
+    public void HandleCamera(Vector2 input)
+    {
+        FollowPlayer();
+        RotateCamera(input);
+        CollideCamera();
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 playerPosition = Vector3.SmoothDamp(transform.position, _player.transform.position, ref _followSpeed, _followTime);
+        transform.position = playerPosition;
+    }
+
+    private void RotateCamera(Vector2 input)
+    {
+        _rotateY += input.x * _cameraRotateSpeed;
+        _rotateX += -input.y * _cameraRotateSpeed;
+        _rotateX = Mathf.Clamp(_rotateX, _minYAngle, _maxYAngle);
+
+        Vector3 rotation = Vector3.zero;
+        rotation.y = _rotateY;
+        Quaternion targetRotation = Quaternion.Euler(rotation);
+        transform.rotation = targetRotation;
         
-        // camera movement
-        transform.position = _desiredPos;
-        _targetPos = _player.transform.position + new Vector3(0, 1.5f, 0);
-        transform.LookAt(_targetPos);
+        rotation = Vector3.zero;
+        rotation.x = _rotateX;
+        targetRotation = Quaternion.Euler(rotation);
+        _cameraPivot.transform.localRotation = targetRotation;
+    }
+
+    private void CollideCamera()
+    {
+        float targetPosition = _defaultZPosition;
+        RaycastHit hit;
+        Vector3 dir = _camera.transform.position - _cameraPivot.transform.position;
+        dir.Normalize();
+
+        if (Physics.SphereCast(_cameraPivot.transform.position, _cameraCollisionRadius, dir, out hit, Mathf.Abs(targetPosition), CollisionLayers))
+        {
+            float distance = Vector3.Distance(_cameraPivot.transform.position, hit.point);
+            targetPosition -= -(distance - _cameraCollisionOffset);
+        }
+
+        if (Mathf.Abs(targetPosition) < _minimumCollisionOffset)
+        {
+            targetPosition -= _minimumCollisionOffset;
+        }
+
+        _cameraVectorPosition.z = Mathf.Lerp(_camera.transform.position.z, targetPosition, 1.0f);
+        _camera.transform.localPosition = _cameraVectorPosition;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     // public
-    public float speed = 10;
+    public float movementSpeed = 10;
+    public float rotationSpeed = 0.1f;
     
     public Vector2 look;
-    
+
     // private
     private PlayerControls _controls;
     private Vector2 _move;
+    private Vector3 _moveDirection;
+    
+    private GameObject _cameraManager;
+    private GameObject _camera;
 
     private void OnEnable()
     {
@@ -28,33 +34,64 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         _controls = new PlayerControls();
-        
+
         _controls.Player.Move.performed += ctx => _move = ctx.ReadValue<Vector2>();
         _controls.Player.Move.canceled += ctx => _move = Vector2.zero;
 
         _controls.Player.Look.performed += ctx => look = ctx.ReadValue<Vector2>();
         _controls.Player.Look.canceled += ctx => look = Vector2.zero;
+
+        _cameraManager = GameObject.FindGameObjectWithTag("Camera Manager");
+        _camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     private void FixedUpdate()
     {
         // movement
-        Vector3 movement = new Vector3(_move.x, 0.0f, _move.y) * speed * Time.deltaTime;
-        transform.Translate(movement, Space.Self);
+        HandleRotation();
+        HandleMovement();
         
-        // rotation
-        if (look.x > 0.3)
-        {
-            transform.Rotate(new Vector3(0, 5, 0));
-        }
-        else if (look.x < -0.3)
-        {
-            transform.Rotate(new Vector3(0, -5, 0));
-        }
+        //Vector3 movement = new Vector3(_move.x, 0.0f, _move.y) * movementSpeed * Time.deltaTime;
+        //transform.Translate(movement, Space.Self);
+        
+        // camera
+        //cameraInputX = look.x;
+        //cameraInputY = look.y;
+
     }
 
-    void Update()
+    private void HandleMovement()
     {
-        
+        _moveDirection = _camera.transform.forward * _move.y;
+        _moveDirection += _camera.transform.right * _move.x;
+        _moveDirection.Normalize();
+        _moveDirection.y = 0;
+
+        Vector3 movementVelocity = _moveDirection * movementSpeed;
+        GetComponent<Rigidbody>().velocity = movementVelocity;
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 targetDirection = Vector3.zero;
+        targetDirection = _camera.transform.forward * _move.y;
+        targetDirection += _camera.transform.right * _move.x;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+    }
+
+    private void LateUpdate()
+    {
+        _cameraManager.GetComponent<CameraScript>().HandleCamera(look);
     }
 }
